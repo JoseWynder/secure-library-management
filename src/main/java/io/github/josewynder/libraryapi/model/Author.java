4f7bc7853b7a8b1d242a697a7eb68b10f03d9eda
Jose Wynder <wynder.jose@gmail.com>
2025-12-31 17:52:19 -0300

    lesson(4.53): How to map JPA entities

---
+++ src/main/java/io/github/josewynder/libraryapi/model/Author.java
@@ -0,0 +1,40 @@
+package io.github.josewynder.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "author", schema = "public")
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private int id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "nationality", length = 50, nullable = false)
+    private String nationality;
+
+    @Deprecated
+    public Author() {
+        // For the use of the framework
+    }
+
+    public Author(String name, LocalDate birthDate, String nationality) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.nationality = nationality;
+    }
+
+}
