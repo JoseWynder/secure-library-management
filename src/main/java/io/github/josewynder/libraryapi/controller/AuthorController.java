7e490daff9f31b5ec9a0a2ce53b6a55067c41bd4
Jose Wynder <wynder.jose@gmail.com>
2026-01-04 18:12:01 -0300

    lesson(5.79): Starting the API coding

---
+++ src/main/java/io/github/josewynder/libraryapi/controller/AuthorController.java
@@ -0,0 +1,10 @@
+package io.github.josewynder.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authors")
+// http://localhost:8080/authors
+public class AuthorController {
+}
