8ee27ba96b20ae786cc208f8d1cb8be1a3df7aeb
Jose Wynder <wynder.jose@gmail.com>
2026-01-04 20:35:04 -0300

    lesson(5.82): Adding JPA auditing to entities

--- src/main/java/io/github/josewynder/libraryapi/model/Author.java
+++ src/main/java/io/github/josewynder/libraryapi/model/Author.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +18,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString(exclude = "books")
+@EntityListeners(AuditingEntityListener.class)
 public class Author {
 
     @Deprecated
@@ -43,4 +48,15 @@ public class Author {
 
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Book> books;
+
+    @CreatedDate
+    @Column(name = "registration_date")
+    private LocalDateTime registrationDate;
+
+    @LastModifiedDate
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
+
+    @Column(name = "user_id")
+    private UUID userId;
 }
