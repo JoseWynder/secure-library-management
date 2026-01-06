aeb5fa8fa2e20350e7a866c9e64ac335d7dc3250
Jose Wynder <wynder.jose@gmail.com>
2026-01-06 19:05:53 -0300

    lesson(7.98): Endpoint for book registration with Bean Validation

--- src/main/java/io/github/josewynder/libraryapi/controller/BookController.java
+++ src/main/java/io/github/josewynder/libraryapi/controller/BookController.java
@@ -1,7 +1,19 @@
 package io.github.josewynder.libraryapi.controller;
 
+import io.github.josewynder.libraryapi.controller.dto.BookRequestDTO;
+import io.github.josewynder.libraryapi.controller.dto.ErrorField;
+import io.github.josewynder.libraryapi.controller.dto.ResponseError;
+import io.github.josewynder.libraryapi.exceptions.DuplicateRegistrationException;
+import io.github.josewynder.libraryapi.model.Author;
+import io.github.josewynder.libraryapi.model.Book;
+import io.github.josewynder.libraryapi.repository.BookRepository;
 import io.github.josewynder.libraryapi.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +23,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     private final BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody @Valid BookRequestDTO book) {
+        try {
+            // map dto to entity
+            // send the entity to service to validate and saving in the database
+            // create a url to access the book data
+            // return code created with header location
+            return ResponseEntity.ok(book);
+        } catch (DuplicateRegistrationException e) {
+            ResponseError errorDTO = ResponseError.conflict(e.getMessage());
+            return ResponseEntity.status(errorDTO.status()).body(errorDTO);
+        }
+    }
 }
--- src/main/java/io/github/josewynder/libraryapi/controller/dto/BookRequestDTO.java
+++ src/main/java/io/github/josewynder/libraryapi/controller/dto/BookRequestDTO.java
@@ -1,17 +1,27 @@
 package io.github.josewynder.libraryapi.controller.dto;
 
 import io.github.josewynder.libraryapi.model.BookGenre;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record BookRequestDTO(
+        @ISBN
+        @NotBlank(message = "required field")
         String isbn,
+        @NotBlank(message = "required field")
         String title,
+        @NotNull(message = "required field")
+        @Past(message = "It cannot be a future date")
         LocalDate publicationDate,
         BookGenre genre,
         BigDecimal price,
+        @NotNull(message = "required field")
         UUID authorId) {
 
 }
