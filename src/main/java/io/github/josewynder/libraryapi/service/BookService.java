fc131b90177d4def0867ff2c4d9849a1636b5469
Jose Wynder <wynder.jose@gmail.com>
2026-01-08 18:28:30 -0300

    lesson(7.106): Understanding what a Spring Data JPA Specification is

--- src/main/java/io/github/josewynder/libraryapi/service/BookService.java
+++ src/main/java/io/github/josewynder/libraryapi/service/BookService.java
@@ -1,10 +1,14 @@
 package io.github.josewynder.libraryapi.service;
 
 import io.github.josewynder.libraryapi.model.Book;
+import io.github.josewynder.libraryapi.model.BookGenre;
 import io.github.josewynder.libraryapi.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -25,6 +29,11 @@ public class BookService {
 
     public void delete(Book book) {
         bookRepository.delete(book);
+    }
+
+    public List<Book> search(String isbn, String authorName, BookGenre genre, LocalDate birthDate) {
+        Specification<Book> specification = null;
+        return bookRepository.findAll(specification);
     }
 
 }
