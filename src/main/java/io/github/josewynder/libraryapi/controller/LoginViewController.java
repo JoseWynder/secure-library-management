6c51ca40711e411272a0b5e6b66cca9f428ccee6
Jose Wynder <wynder.jose@gmail.com>
2026-01-12 16:30:06 -0300

    lesson(9.128): Enabling Social Login and Authenticating with a Google Account

--- src/main/java/io/github/josewynder/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/josewynder/libraryapi/controller/LoginViewController.java
@@ -1,7 +1,9 @@
 package io.github.josewynder.libraryapi.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginViewController {
@@ -9,5 +11,11 @@ public class LoginViewController {
     @GetMapping("/login")
     public String loginPage() {
         return "login";
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String homePage(Authentication authentication) {
+        return "Hello " + authentication.getName();
     }
 }
