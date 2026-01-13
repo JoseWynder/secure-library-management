7f0f9c8583c3277296ab28670ea290b78c0b4c95
Jose Wynder <wynder.jose@gmail.com>
2026-01-13 19:45:29 -0300

    lesson(9.133): Creating a strategy to unify the Google user

--- src/main/java/io/github/josewynder/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/josewynder/libraryapi/controller/LoginViewController.java
@@ -1,5 +1,6 @@
 package io.github.josewynder.libraryapi.controller;
 
+import io.github.josewynder.libraryapi.security.CustomAuthentication;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,9 @@ public class LoginViewController {
     @GetMapping("/")
     @ResponseBody
     public String homePage(Authentication authentication) {
+        if(authentication instanceof CustomAuthentication customAuthentication) {
+            System.out.println(customAuthentication.getUser());
+        }
         return "Hello " + authentication.getName();
     }
 }
