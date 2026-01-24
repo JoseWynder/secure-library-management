a3b7e72fcbc2e1c2e0da0d0e3fd286c142d137dc
Jose Wynder <wynder.jose@gmail.com>
2026-01-24 18:52:26 -0300

    lesson(10.151): Authenticating with Authorization Code grant type

--- src/main/java/io/github/josewynder/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/josewynder/libraryapi/controller/LoginViewController.java
@@ -4,6 +4,7 @@ import io.github.josewynder.libraryapi.security.CustomAuthentication;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -21,5 +22,11 @@ public class LoginViewController {
             System.out.println(customAuthentication.getUser());
         }
         return "Hello " + authentication.getName();
+    }
+
+    @GetMapping("/authorized")
+    @ResponseBody
+    public String getAuthorizationCode(@RequestParam("code") String code) {
+        return "Your authorization code: " + code;
     }
 }
