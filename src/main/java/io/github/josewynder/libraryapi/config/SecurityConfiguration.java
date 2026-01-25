62bc8678f48ba5e890f3a8c5818e8808057ba074
Jose Wynder <wynder.jose@gmail.com>
2026-01-25 15:52:17 -0300

    lesson(10.154): Customizing the JwtAuthenticationToken for CustomAuthentication

--- src/main/java/io/github/josewynder/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/josewynder/libraryapi/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package io.github.josewynder.libraryapi.config;
 
+import io.github.josewynder.libraryapi.security.JwtCustomAuthenticationFilter;
 import io.github.josewynder.libraryapi.security.LoginSocialSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,7 +24,10 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http,
-            LoginSocialSuccessHandler successHandler) throws Exception {
+            LoginSocialSuccessHandler successHandler,
+            JwtCustomAuthenticationFilter jwtCustomAuthenticationFilter
+    ) throws Exception {
+
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
@@ -42,6 +47,9 @@ public class SecurityConfiguration {
                 })
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(Customizer.withDefaults()))
+                .addFilterAfter(
+                        jwtCustomAuthenticationFilter,
+                        BearerTokenAuthenticationFilter.class)
                 .build();
     }
 
